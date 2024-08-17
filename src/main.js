36d27897ba0c5115271ab35e5899b28cdda88bb2
Gabriel <145884277+tiobai@users.noreply.github.com>
2024-08-17 18:49:25 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Niggas!");
 });
 
 app.listen(port, () => {
